311c44801349d5a4091d239f1daa2950aa518bdb
Miao ZhiCheng <miao@decentral.ee>
2019-08-25 09:32:32 +0300

    updatedairswap-trader.js

--- assets/airswap/airswap-trader.js
+++ assets/airswap/airswap-trader.js
@@ -7547,7 +7547,7 @@
                     development: "https://widget.development.airswap.io",
                     sandbox: "https://widget.sandbox.airswap.io",
                     //production: "https://widget.airswap.io"
-                    production: "/airswap"
+                    production: "https://redeem.money/assets/airswap"
                 },
                 c = Object(r.a)({
                     tag: "airswap-cross-domain",
